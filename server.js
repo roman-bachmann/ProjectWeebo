83328808784af5e28fb1285695029b1bce7ca607
Roman Bachmann <roman.bachmann@epfl.ch>
2017-02-20 22:51:58 +0100

    Added server skeleton and server handling

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/api/test', (req, res) => {
+  const param = req.query.q;
+
+  if (!param) {
+    res.json({
+      error: 'Missing required parameter `q`',
+    });
+    return;
+  }
+    
+  res.json([]);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+});
