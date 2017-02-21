f4aba49c7c13fbd8f092eda21fbde05a0016b525
Sølve Robert Bø Hunvik <srhunvik@stud.ntnu.no>
2017-02-21 14:25:21 +0100

    Structuring and getting ready for backend

--- client/src/subject/chapter/ChapterView.js
+++ client/src/subject/chapter/ChapterView.js
@@ -10,51 +10,55 @@ var Nav = require("react-bootstrap/lib/Nav");
 var Accordion = require('../subchapter/Accordion.js');
 
 var Tabs = React.createClass({
+    getInitialState: function () {
+      return { data: {comments: []} };
+    },
+
+    loadCommentsFromServer: function () {
+      var chapters = ['Problems and algorithms', 'Datastructures', 'Split and conquer', 'Ranking in linear time'];
+      this.setState({
+        data: chapters
+      });
+      console.log("mounted");
+    },
+
+    
+    componentWillMount: function (){
+      this.loadCommentsFromServer()
+    },
+
     render: function () {
-        return (
-    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-    <Row className="clearfix">
-      <Col sm={3}>
-        <Nav bsStyle="pills" stacked>
-          <NavItem eventKey="first">
-            Problems and algorithms
-          </NavItem>
-          <NavItem eventKey="second">
-            Datastructures
-          </NavItem>
-            <NavItem eventKey="third">
-            Split and conquer
-          </NavItem>
-            <NavItem eventKey="fourth">
-            Ranking in linear time
-          </NavItem>
-            <NavItem eventKey="fifth">
-            Rooted trees
-          </NavItem>
-        </Nav>
-      </Col>
-      <Col sm={9}>
-        <Tab.Content animation>
-          <Tab.Pane eventKey="first" >
-            <Accordion />
-          </Tab.Pane>
-          <Tab.Pane eventKey="second">
-            Tab 2 content
-          </Tab.Pane>
-            <Tab.Pane eventKey="third">
-            Tab 3 content
-          </Tab.Pane>
-            <Tab.Pane eventKey="fourth">
-            Tab 4 content
-          </Tab.Pane>
-            <Tab.Pane eventKey="fifth">
-            Tab 5 content
-          </Tab.Pane>
-        </Tab.Content>
-      </Col>
-    </Row>
-  </Tab.Container>
-        )
+      if(this.state.data){
+        var chaptersList = this.state.data.map(function (name, index){
+          return <NavItem eventKey={'num' + index}>{name}</NavItem>;
+        });
+      }
+
+      return (
+        <Tab.Container id="left-tabs-example" defaultActiveKey="num0">
+          <Row className="clearfix">
+            <Col sm={3}>
+              <Nav bsStyle="pills" stacked>{chaptersList}</Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content animation>
+                <Tab.Pane eventKey="num0" >
+                  <Accordion />
+                </Tab.Pane>
+                <Tab.Pane eventKey="num1">
+                  Tab 2 content
+                </Tab.Pane>
+                  <Tab.Pane eventKey="num2">
+                  Tab 3 content
+                </Tab.Pane>
+                  <Tab.Pane eventKey="num3">
+                  Tab 4 content
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      )
     }
 });
 
