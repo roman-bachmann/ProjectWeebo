7780a2b77708a3a38403dd6533b9bffbd5baacb8
Sølve Robert Bø Hunvik <srhunvik@stud.ntnu.no>
2017-03-04 16:12:03 +0100

    Modifications on addvideomodal. Closely finished

--- client/src/subject/subchapter/AddVideoModal.js
+++ client/src/subject/subchapter/AddVideoModal.js
@@ -1,23 +1,42 @@
 var React = require('react');
 import {Modal, Button} from 'react-bootstrap';
+var YouTube = require('./YouTubePlayer.js');
+import './AddVideoModal.css';
 
 const MySmallModal = React.createClass({
+  getInitialState: function () {
+    return {
+      userInput: ''
+    };
+  },
+
+   handleUserInput: function(e) {
+    var videoSplit = e.target.value.split("=");
+    var videoWithoutExtras = videoSplit[1].split("&");
+    var videoId = videoWithoutExtras[0];
+    this.setState({
+      userInput: videoId
+    });
+  },
+
+  handleShare: function() {
+    console.log(this.state.userInput);
+  },
+
   render() {
     return (
-      <Modal {...this.props} bsSize="small" aria-labelledby="contained-modal-title-sm">
+      <Modal {...this.props} bsSize="medium" aria-labelledby="contained-modal-title-sm">
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-sm">Modal heading</Modal.Title>
+          <Modal.Title id="contained-modal-title-sm">Add video</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Add video</h4>
-          <p>Subject: {this.props.subject}</p>
-          <p>Chapter: {this.props.chapter}</p>
-          <p>Subchapter: {this.props.subchapter}</p>
-          
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          <p>Subject: {this.props.subject} Chapter: {this.props.chapter} Subchapter: {this.props.subchapter}</p>
+          <p>YouTube link: <input type="text" onChange={this.handleUserInput}/></p>
+          <YouTube id={this.state.userInput}/>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={this.props.onHide}>Close</Button>
+          <Button className="closeBtn" onClick={this.props.onHide}>Close</Button>
+          <Button className="acceptBtn" onClick={this.handleShare}>Share</Button>
         </Modal.Footer>
       </Modal>
     );
