48a04e8b065448f0a23165ff964ead15a5d9cb08
Sølve Robert Bø Hunvik <srhunvik@stud.ntnu.no>
2017-03-06 15:39:22 +0100

    Added possibility to add vimeo

--- client/src/subject/subchapter/AddVideoModal.js
+++ client/src/subject/subchapter/AddVideoModal.js
@@ -7,7 +7,8 @@ import Client from '../../Client.js';
 const MySmallModal = React.createClass({
   getInitialState: function () {
     return {
-      userInput: ''
+      userInput: '',
+      sharingsite: 'YouTube',
     };
   },
 
@@ -34,14 +35,24 @@ const MySmallModal = React.createClass({
   },
 
   handleShare: function() {
-    console.log(this.state.userInput);
-    var userID = 'frodo';
-    var subjectID = this.props.subject;
-    var chapterID = this.props.chapter;
-    var subChapterID = this.props.subchapter;
-    var videoID = this.state.userInput;
-    Client.videoShare(userID, subjectID, chapterID, subChapterID, videoID);
-    this.props.onHide();
+    if(this.state.sharingsite === 'YouTube'){
+      console.log(this.state.userInput);
+      var userID = 'frodo';
+      var subjectID = this.props.subject;
+      var chapterID = this.props.chapter;
+      var subChapterID = this.props.subchapter;
+      var videoID = this.state.userInput;
+      Client.videoShare(userID, subjectID, chapterID, subChapterID, videoID);
+      this.props.onHide();
+    }else{
+      this.props.onHide();
+    }
+  },
+  videoSiteChange: function(e) {
+    this.setState({
+      sharingsite: e.target.value
+    });
+    this.handleUserInput();
   },
 
   render() {
@@ -51,8 +62,13 @@ const MySmallModal = React.createClass({
           <Modal.Title id="contained-modal-title-sm">Add video</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>YouTube link/id: <input type="text" onChange={this.handleUserInput}/></p>
-          <YouTube id={this.state.userInput}/>
+          <p><span>YouTube </span><input name="sharingsite" type="radio" value="YouTube" onChange={this.videoSiteChange} checked={this.state.sharingsite === 'YouTube'}/> 
+           <span> Vimeo </span><input name="sharingsite" type="radio" value="Vimeo" onChange={this.videoSiteChange} checked={this.state.sharingsite === 'Vimeo'}/>
+          </p>
+          <p>{this.state.sharingsite} link/id: <input type="text" onChange={this.handleUserInput}/></p>
+          {this.state.sharingsite === 'YouTube' ? 
+            <YouTube id={this.state.userInput}/>
+          :<h3 style={{color: 'orange'}}>Vimeo will be supported later.</h3>}
           <p><span style={{color: 'red'}}>Notice: </span>You need to reopen your active panel to see the video you shared.</p>
         </Modal.Body>
         <Modal.Footer>
