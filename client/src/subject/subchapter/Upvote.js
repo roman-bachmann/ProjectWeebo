c571fdeae3f95af5c570ef98db81e1a543e52028
Sølve Robert Bø Hunvik <srhunvik@stud.ntnu.no>
2017-03-09 13:10:04 +0100

    Finished voting system. Buttons is now shown as clicked.

--- client/src/subject/subchapter/Upvote.js
+++ client/src/subject/subchapter/Upvote.js
@@ -3,33 +3,50 @@ import {Grid, Row, Col} from 'react-bootstrap';
 var React = require('react');
 import Client from '../../Client.js';
 
+var ColorsVote = { 	"Upgreen": "#81b71a",
+                    "Downred": "#cd5c5c",
+                    "BgUnclicked": "#efefef",
+                    "BgClicked": '#7f7c7c',
+                    
+};
+
 var Upvote = React.createClass({
 	getInitialState: function () {
 		//fetch from database here
 		return {
 			votes: 0,
-			 users: [],
-			 containsUser: false
+			 users: {},
+			 containsUser: false,
+			 upVoteColor: ColorsVote.Upgreen,
+			 downVoteColor: ColorsVote.Downred,
+			 bgUp: ColorsVote.BgUnclicked,
+			 bgDown: ColorsVote.BgUnclicked
 			};
 	},
 	loadVotesFromServer: function (videoID) {
 		Client.getVotesForVideo(videoID, (voteData) => {
 			console.log("done fetching.");
 			if (voteData) {
-				var usersList = [];
+				var usersDict = {};
 				var counts = 0;
 				for (var i = 0; i < voteData.length; i++) {
 					counts += voteData[i].rating_score;
-					usersList.push(voteData[i].userID);
+					usersDict[voteData[i].userID] = voteData[i].rating_score;
 				}
 				var userID = this.props.userID;
 				var containsUserValue = false;
-				for(var i = 0; i < usersList.length; i++){
-					if(usersList[i]==userID){
-						containsUserValue = true;
+				
+				if(userID in usersDict){
+					containsUserValue = true;
+					if(usersDict[userID] == 1){
+						this.setState({ votes: counts, users: usersDict, containsUser: containsUserValue, bgUp: ColorsVote.BgClicked});
+					}else{
+						this.setState({ votes: counts, users: usersDict, containsUser: containsUserValue, bgDown: ColorsVote.BgClicked});
 					}
 				}
-				this.setState({ votes: counts, users: usersList, containsUser: containsUserValue});
+				else{
+					this.setState({ votes: counts, users: usersDict, containsUser: containsUserValue});
+				}
 			}
 		});
     },
@@ -42,17 +59,16 @@ var Upvote = React.createClass({
 			this.loadVotesFromServer(nextProps.videoID);
 		}
 	},
-
 	addVote: function () {
 		//if user haven't already voted add vote and update database. 
 		if(this.state.containsUser == false){
 			var d = new Date().toISOString().slice(0, 19).replace('T', ' ');
-			console.log(d);
 			Client.videoVote(this.props.userID, this.props.videoid, 1, d);
 			var newVote = this.state.votes + 1;
 			this.setState({
 				votes: newVote,
-				containsUser: true
+				containsUser: true,
+				bgUp: '#7f7c7c'
 			});
 		}
 	},
@@ -60,23 +76,24 @@ var Upvote = React.createClass({
 		//check authentication and update database here
 		if(this.state.containsUser == false){
 			var d = new Date().toISOString().slice(0, 19).replace('T', ' ');
-			console.log(d);
 			Client.videoVote(this.props.userID, this.props.videoid, -1, d);
 			var newVote = this.state.votes - 1;
 			this.setState({
 				votes: newVote,
-				containsUser: true
+				containsUser: true,
+				bgDown: '#7f7c7c'
+
 			});
 		}
 	},
 	render: function() {
 		return (
 			<div className="upVoteWrap"> 
-					<button className="upvoteBtn" onClick={this.addVote}>▲</button>
+					<button className="upvoteBtn" onClick={this.addVote} style={{color: this.state.upVoteColor, backgroundColor: this.state.bgUp}}>▲</button>
 					<div className="numDiv">
 						<p className="votenumber">{this.state.votes}</p>
 					</div>
-					<button className="downvoteBtn" onClick={this.removeVote}>▼</button>
+					<button className="downvoteBtn" onClick={this.removeVote} style={{color: this.state.downVoteColor, backgroundColor: this.state.bgDown}}>▼</button>
 			</div>
 		);
 	}
