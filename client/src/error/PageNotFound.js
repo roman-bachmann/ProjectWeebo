b920e6a35b23706c7a2f36dcb854164b4c0fd487
Roman Bachmann <roman.bachmann@epfl.ch>
2017-03-10 15:27:01 +0100

    Added name to profile

--- client/src/error/PageNotFound.js
+++ client/src/error/PageNotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export class PageNotFound extends React.Component {
   render() {
     return (
         <div className="AdminBody">
             <h1>Error 404 - Page not found!</h1>
+            <Link to={'/home'}>Back to Home</Link>
         </div>
     )
   }
--- client/src/profile/ProfileDetails.js
+++ client/src/profile/ProfileDetails.js
@@ -9,14 +9,14 @@ export class ProfileDetails extends React.Component {
 
   render() {
     const { profile } = this.props
-    const { address } = profile.user_metadata || {}
+    const { first_name, last_name } = profile.user_metadata || {}
     return (
       <Row className="ProfileRow">
         <Col md={2} mdOffset={4}>
           <Image className="profileimg" src={profile.picture} circle/>
         </Col>
         <Col md={6}>
-          <p className="ProfileName">{profile.name}</p>
+          <p className="ProfileName">{first_name} {last_name}</p>
           <p className="ProfileDetails"><strong>Email: </strong> {profile.email}</p>
           <p className="ProfileDetails"><strong>Nickname: </strong> {profile.nickname}</p>
         </Col>
