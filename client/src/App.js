ed7bebdb91d7e2995d45f1c253d2b76623ddef8b
Roman Bachmann <roman.bachmann@epfl.ch>
2017-03-17 16:56:46 +0100

    Fixed footer links

--- client/src/App.js
+++ client/src/App.js
@@ -83,7 +83,7 @@ export class App extends React.Component {
                     {childrenWithProps}
                 </div>
 
-                <Footer />
+                <Footer auth={this.props.route.auth} />
 
             </div>
         );
