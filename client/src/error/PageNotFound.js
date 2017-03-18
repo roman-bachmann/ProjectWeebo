6f244ccc900ff63d1b0b3cf56b4cc4a607ca74db
Thay <thayanan@gmail.com>
2017-03-18 12:44:38 +0100

    Styled the error page.

--- client/src/error/PageNotFound.js
+++ client/src/error/PageNotFound.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router'
+import './PageNotFound.css'
 
 export class PageNotFound extends React.Component {
   render() {
     return (
-        <div className="AdminBody">
-            <h1>Error 404 - Page not found!</h1>
-            <Link to={'/home'}>Back to Home</Link>
+        <div className="PageNotFound">
+            <h1 className="PageNotFoundTitle">Error 404 - Page not found!</h1>
+            <div className="LogoWrap"><h1 className="W_Logo">W</h1></div>
+            <Link to={'/home'} className="ReturnHome">Return to Homepage</Link>
         </div>
     )
   }
