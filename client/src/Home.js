361a766c2e374fd3b59d5851145784f9b389b169
Thay <thayanan@gmail.com>
2017-03-19 09:29:54 +0100

    Tried some new background images for the login page.

--- client/src/Home.js
+++ client/src/Home.js
@@ -24,6 +24,7 @@ var Home = React.createClass({
           <div>
             <div className="title">
               <h1 className="Welcome">Welcome!</h1>
+              <hr className="LoginHR"></hr><hr className="LoginHR2"></hr>
                 <div className="LogoWrap"><h1 className="W_Logo">W</h1></div>
             </div>
             <div className="content">
