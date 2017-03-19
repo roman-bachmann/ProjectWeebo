75c49312f05c38797a244cdb04bc8d0ad3c7b287
Thay <thayanan@gmail.com>
2017-03-19 23:22:01 +0100

    Changed the login text.

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -25,34 +25,33 @@ export class Login extends React.Component {
         <div className="featuresSection">
   		<h3 className="FeaturesTitle">Features</h3>
       <hr className="LoginHR"></hr><hr className="LoginHR2"></hr>
-  		<p className="FeaturesDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed aliquam dictum sapien, id sagittis augue malesuada eu.</p>
+  		<p className="FeaturesDescription">With Weebo, students are able to watch, rate and share the best educational videos for a specific subject. Professors are then able to verify the quality of content, recommend videos, or even publish an own video. Weebo will be the future for finding educational videos for any subject, independent of university or school. Wasting time on searching videos on Youtube is a past!</p>
   		<ul className="grid">
   			<li>
           <Glyphicon glyph="glyphicon glyphicon-thumbs-up" className="Glyphicon-Login"/>
-  				<h4 className="MainFeatures">Rating system</h4>
-  				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
+  				<h4 className="MainFeatures">Rating System</h4>
+  				<p>A user can easily find the best rated videos on Weebo.</p>
   			</li>
   			<li>
           <Glyphicon glyph="glyphicon glyphicon-user"  className="Glyphicon-Login"/>
   				<h4 className="MainFeatures">Professor Panel</h4>
-  				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
+  				<p>Professors have access to an admin panel for adding subject, recommend video or even remove unwanted content.</p>
   			</li>
   			<li>
-          <Glyphicon glyph="glyphicon glyphicon-pencil"  className="Glyphicon-Login"/>
-  				<h4 className="MainFeatures">Profile Page</h4>
-  				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices vitae pulvinar velit.</p>
+          <Glyphicon glyph="glyphicon glyphicon-heart-empty"  className="Glyphicon-Login"/>
+  				<h4 className="MainFeatures">Favorite</h4>
+  				<p>A user can easily find favorite a video to watch it later.</p>
   			</li>
       </ul>
   		</div>
         <div className="featuresSection">
       <div className="features2">
-		<h3 className="FeaturesTitle">What others say</h3>
+		<h3 className="FeaturesTitle">What professors say</h3>
 
     <hr className="LoginHR"></hr><hr className="LoginHR2"></hr>
 <img className="Pekka" src="https://innsidawls.itea.ntnu.no/user-profile-service/rest/files/0db9c213-65f8-3a6e-a03d-b4d3bc533030"></img>
-		<p className="quote">Mauris sit amet mauris a arcu eleifend ultricies eget ut dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-		<p className="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id felis et ipsum bibendum ultrices. Morbi vitae pulvinar velit. Sed aliquam dictum sapien, id sagittis augue malesuada eu.</p>
-		<p className="author">— Pekka</p>
+		<p className="quote">Weebo is an amazing product, delivered by young talents from Trondheim in Norway. I am really proud to be a part of their journey and look forward to more features in Weebo.</p>
+		<p className="author">— Pekka Kalevi Abrahamsson</p>
 	</div>
   </div>
 
