3e2bee326d7c65dd3064adbd382f2b3f0992321e
Thay <thayanan@gmail.com>
2017-03-20 15:55:05 +0100

    Redesigned the Edit courses on Profile page

--- client/src/profile/CourseList.js
+++ client/src/profile/CourseList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Button, Glyphicon} from 'react-bootstrap';
 import Client from '../Client.js';
+import './CourseList.css'
 
 var CourseList = React.createClass({
 
@@ -12,16 +13,17 @@ var CourseList = React.createClass({
 
     render: function () {
         var courseItems = this.props.courses.map((c, idx) => (
-            <div>
-                {c.subjectID + " - " + c.name}
-                <Button onClick={() => this.deleteCourseForUser(c.subjectID, this.props.userID)}>
-                    <Glyphicon glyph="glyphicon glyphicon-trash"/>
+              <div>
+                {c.subjectID + " - " + c.name + "  "}
+                <Button className="removeCourseButton" onClick={() => this.deleteCourseForUser(c.subjectID, this.props.userID)}>
+                    <Glyphicon glyph="glyphicon glyphicon-remove"/>
                 </Button>
             </div>
         ));
 
         return (
             <div>
+              <div className="EditCoursesTitle">Edit your subjects</div>
                 {courseItems}
             </div>
         )
