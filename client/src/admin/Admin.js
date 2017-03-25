0ede4d8fcb398e88466978de8e2f5ee285c8d441
Thay <thayanan@gmail.com>
2017-03-25 09:45:12 +0100

    Redesigned the add/delete-course-widget

--- client/src/admin/Admin.js
+++ client/src/admin/Admin.js
@@ -38,32 +38,34 @@ var Admin = React.createClass({
             <div className="AdminBody">
                   <h2 className="animated adminTitle"> Admin Page</h2>
 <div><hr className="animated ProfileHR"></hr><hr className="animated ProfileHR2"></hr></div>
+        <div className="animated addDeleteCourseWidgets">
                 <div className=" animated AddCoursewidget">
                   <div className="addCoursecover">
                     <img src="https://s-media-cache-ak0.pinimg.com/originals/f0/b9/52/f0b9523dacbecc5bd4e2aae496a9c8c2.jpg" />
                   </div>
                 <Image className="addCoursephoto" src={CourseListIcon} circle/>
-                <div className="addACourseTitle">Add a new course</div>
+                <div className="addACourseTitle">Add courses</div>
                 <AdminAddCourse reloadAllCourses={this.loadAllCourses}/>
                   <ul className="addCourseProfileUL">
                     <li className="addCourseProfileULLI"><Glyphicon className="Userglyph" glyph="glyphicon glyphicon-plus-sign"/></li>
                   </ul>
                 </div>
 
-                <div className=" animated Coursewidget">
-                  <div className="Coursecover">
+                <div className=" animated deleteCoursewidget">
+                  <div className="deleteCoursecover">
                     <img src="http://www.techandall.com/wp-content/uploads/2013/08/bg6.jpg" />
                   </div>
                 <Image className="deleteCoursephoto" src={CourseListIcon} circle/>
                 <h2 className="DeleteCourses">Delete courses</h2>
-                <p>Delete subjects from the database</p>
+                <p>Delete courses from the database</p>
                 <AdminCourseList courses={this.state.allCourses}
                                  deleteCourse={this.deleteCourse}/>
-                  <ul className="CourseProfileUL">
-                    <li className="CourseProfileULLI"><Glyphicon className="Userglyph" glyph="glyphicon glyphicon-minus-sign"/></li>
+                  <ul className="deleteCourseProfileUL">
+                    <li className="deleteCourseProfileULLI"><Glyphicon className="Userglyph" glyph="glyphicon glyphicon-minus-sign"/></li>
                   </ul>
                 </div>
             </div>
+          </div>
         )
     }
 });
