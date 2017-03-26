45f2acc88b90e8d2b26cf3d9b62f33d2d9abd025
Thay <thayanan@gmail.com>
2017-03-26 20:00:52 +0200

    Added more content to About page.

--- client/src/about/About.js
+++ client/src/about/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router'
 import './About.css'
+import AppImage from '../img/App.png'
 
 export class About extends React.Component {
   render() {
@@ -12,8 +13,7 @@ export class About extends React.Component {
           <div className="container">
     <div className="animated col-md-12 col-sm-12">
       <h2 className="aboutSectionTitle">What Is Weebo?</h2>
-      <p>With Weebo, students are able to watch, rate and share the best educational videos for a specific subject. Professors are then able to verify the quality of content, recommend videos, or even publish an own video. Weebo will be the future for finding educational videos for any subject, independent of university or school. Wasting time on searching videos on Youtube is a past!</p>
-
+      <p className="aboutWeebo">With Weebo, students are able to watch, rate and share the best educational videos for a specific subject. Professors are then able to verify the quality of content, recommend videos, or even publish an own video. Weebo will be the future for finding educational videos for any subject, independent of university or school. Wasting time on searching videos on Youtube is a past!</p>
     </div>
     <div className="animated col-md-4 col-sm-4">
       <span className="aboutIcons"><i className="fa fa-film"></i></span>
@@ -38,20 +38,21 @@ export class About extends React.Component {
         <div className="service">
           <div className="container">
     <div className="animated col-md-12 col-sm-12">
-      <h2 className="aboutSectionTitle">Our Workflow</h2>
+      <h2 className="aboutWorkflowSectionTitle">Our Workflow</h2>
+      <p className="aboutWeebo">With Weebo, students are able to watch, rate and share the best educational videos for a specific subject. Professors are then able to verify the quality of content, recommend videos, or even publish an own video. Weebo will be the future for finding educational videos for any subject, independent of university or school. Wasting time on searching videos on Youtube is a past!</p>
     </div>
     <div className="animated col-md-4 col-sm-4">
-      <span className="aboutIcons"><i className="fa fa-cubes"></i></span>
-      <h3 className="animatedserviceTitle">Research</h3>
+      <span className="aboutWorkflowIcons"><i className="fa fa-cubes"></i></span>
+      <h3 className="animated serviceTitle">Research</h3>
       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
     </div>
     <div className="animated col-md-4 col-sm-4">
-      <span className="aboutIcons"><i className="fa fa-cogs"></i></span>
+      <span className="aboutWorkflowIcons"><i className="fa fa-cogs"></i></span>
       <h3 className="animated serviceTitle">Design</h3>
       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
     </div>
     <div className="animated col-md-4 col-sm-4">
-      <span className="aboutIcons"><i className="fa fa-group"></i></span>
+      <span className="aboutWorkflowIcons"><i className="fa fa-group"></i></span>
       <h3 className="animated serviceTitle">Implement</h3>
       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
     </div>
@@ -60,6 +61,44 @@ export class About extends React.Component {
   <div>
     <hr className="animated aboutSectionHr"/>
   </div>
+
+  <div className="service">
+    <div className="container">
+<div className="animated col-md-12 col-sm-12">
+<h2 className="aboutWorkflowSectionTitle">Future Plans</h2>
+<p className="aboutWeebo">Team Weebo is continiously working on improving the user experience. Our next goal is to make Weebo fully compitable to mobile devices with apps launching for both Andorid and iOS devices as well as Apple TV 4.</p>
+</div>
+<img src={AppImage} className="aboutAppImage"/>
+</div>
+</div>
+<div>
+<hr className="animated aboutSectionHr"/>
+</div>
+
+<div className="service">
+  <div className="container">
+<div className="animated col-md-12 col-sm-12">
+<h2 className="aboutWorkflowSectionTitle">Follow Us</h2>
+<p className="aboutWeebo">Follow us on social media to catch up with the latest news and updates from Team Weebo. Please share Weebo with friends and family if you like the website. Thanks!</p>
+</div>
+<div className="animated col-md-4 col-sm-4">
+  <span className="aboutIcons"><i className="fa fa-facebook"></i></span>
+  <h3 className="animated serviceTitle">Facebook</h3>
+</div>
+<div className="animated col-md-4 col-sm-4">
+  <span className="aboutIcons"><i className="fa fa-twitter"></i></span>
+  <h3 className="serviceTitle">Twitter</h3>
+</div>
+<div className="animated col-md-4 col-sm-4">
+  <span className="aboutIcons"><i className="fa fa-google-plus"></i></span>
+  <h3 className="serviceTitle">Google Plus</h3>
+</div>
+</div>
+</div>
+<div>
+<hr className="animated aboutSectionHr"/>
+</div>
+
 </div>
     )
   }
