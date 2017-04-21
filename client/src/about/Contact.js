a02050b582c1d7951090a3c3921f06d47ea98cbe
Thay <thayanan@gmail.com>
2017-04-21 12:06:57 +0200

    Added links to contact page

--- client/src/about/Contact.js
+++ client/src/about/Contact.js
@@ -37,9 +37,9 @@ export class Contact extends React.Component {
       						<h4 className="wholeName"><span className="firstname">Sølve </span>Hunvik </h4>
       					</a>
       					<ul>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
-      						<li><a href="#"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-instagram"></i></a></li>
+      						<li><a href="https://www.facebook.com/Solveh?fref=ts" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
+      						<li><a href="https://www.linkedin.com/in/s%C3%B8lve-b%C3%B8-hunvik-a13910120/"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
+      						<li><a href="mailto:srhunvik@stud.ntnu.no" target="_blank" className="teamSocialIcons"><i className="fa fa-envelope"></i></a></li>
       					</ul>
       				</div>
       				<div className="animated TeamRow">
@@ -54,9 +54,9 @@ export class Contact extends React.Component {
       						<h4 className="wholeName"><span className="firstname">Roman</span> Bachmann</h4>
       					</a>
       					<ul>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
-      						<li><a href="#"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-instagram"></i></a></li>
+      						<li><a href="https://www.facebook.com/profile.php?id=100006878116639" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
+      						<li><a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.linkedin.com%2Fin%2Froman-bachmann-818b01141%2F&h=ATMlahvnVVAiCRP71ghgus4DXSMT-iM4AYmpseX5wHpcxgwMr475nSqFo2WteBSs0-zduuFG6nUzOdOuzgVdMb7-0ZflFfqwdJVm9r3YaNME2381xXvLGqkb2PVi70WCUuREgdyC1kTSlOTtRez-LmeBraw"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
+      						<li><a href="mailto:roman.bachmann@epfl.ch" target="_blank" className="teamSocialIcons"><i className="fa fa-envelope"></i></a></li>
       					</ul>
       				</div>
       				<div className="animated TeamRow">
@@ -71,9 +71,9 @@ export class Contact extends React.Component {
       						<h4 className="wholeName"><span className="firstname">Thayanan</span> Tharmapalan</h4>
       					</a>
       					<ul>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
-      						<li><a href="#"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-instagram"></i></a></li>
+      						<li><a href="https://www.facebook.com/thay.tharma" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
+      						<li><a href="https://www.linkedin.com/in/thayanan-tharmapalan/"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
+      						<li><a href="mailto:thayanat@stud.ntnu.no" target="_blank" className="teamSocialIcons"><i className="fa fa-envelope"></i></a></li>
       					</ul>
       				</div>
       				<div className="animated TeamRow">
@@ -88,9 +88,9 @@ export class Contact extends React.Component {
       						<h4 className="wholeName"><span className="firstname">Emil</span> Grip</h4>
       					</a>
       					<ul>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
-      						<li><a href="#"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
-      						<li><a href="#" target="_blank" className="teamSocialIcons"><i className="fa fa-instagram"></i></a></li>
+      						<li><a href="https://www.facebook.com/eegrip?fref=ts" target="_blank" className="teamSocialIcons"><i className="fa fa-facebook"></i></a></li>
+      						<li><a href="https://www.linkedin.com/in/emil-eyser-grip-448a6a76/"  target="_blank" className="teamSocialIcons"><i className="fa fa-linkedin"></i></a></li>
+      						<li><a href="mailto:emileg@stud.ntnu.no" target="_blank" className="teamSocialIcons"><i className="fa fa-envelope"></i></a></li>
       					</ul>
       				</div>
       			</div>
